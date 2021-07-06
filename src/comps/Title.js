576d37807b6cec15bbb856012b0dd42d456c1ceb
shaiksohaib <sm.sohaib01@gmail.com>
2021-07-06 17:33:53 +0530

    adding title.js

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      
+      <h2>imgbum Pictures</h2>
+      <p>Imgbum is an Indian online image sharing community and image host. .</p>
+    </div>
+  )
+}
+
+export default Title;
